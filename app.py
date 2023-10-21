579e87704adde46d0377bf0a8e9298f73859f883
Qiming <99074858+TayQiMing@users.noreply.github.com>
2023-10-21 22:36:37 +0800

    Modified for running app.py
    Modified app.py and succesfully run it but left one problem which is the 'Modeling' part, the best model comparizon didnt shown results, seems like has problem on those changes libraries version and cause the pycaret cant perform compare_model() as expected?

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from operator import index
 import streamlit as st
 import plotly.express as px
-from pycaret.regression import setup, compare_models, pull, save_model, load_model
-import pandas_profiling
+from pycaret.classification import setup, compare_models, pull, save_model, load_model
+# import pandas_profiling
+from ydata_profiling import ProfileReport
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
@@ -26,18 +27,24 @@ if choice == "Upload":
 
 if choice == "Profiling": 
     st.title("Exploratory Data Analysis")
-    profile_df = df.profile_report()
+    # profile_df = df.profile_report()
+    profile_df = ProfileReport(df)
     st_profile_report(profile_df)
 
 if choice == "Modelling": 
+    st.title("Machine Learning Modeling")
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
+        # setup(df, target=chosen_target, silent=True)
+        setup(df, target=chosen_target, verbose=False)
         setup_df = pull()
+        st.info('The ML settings is:')
         st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
+        best_model = compare_models()   # Problem at modeling as the compare model and each model score didnt shown in table, return as empty result
+        compare_df = pull()             # Maybe due to different version of libraries
+        st.info('The best model is:')
         st.dataframe(compare_df)
+        best_model
         save_model(best_model, 'best_model')
 
 if choice == "Download": 
